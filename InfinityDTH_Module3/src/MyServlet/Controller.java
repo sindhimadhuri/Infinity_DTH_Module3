6adb0c1c26d805e202155728276d4d28c3722138
Pavithradm <31830464+Pavithradm@users.noreply.github.com>
2019-11-19 11:47:05 -0500

    controller addition

---
+++ InfinityDTH_Module3/src/MyServlet/Controller.java
@@ -0,0 +1,30 @@
+package MyServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Inside Servlet");
+		String dimension = request.getParameter("dimension");
+		out.println(dimension);
+	}
+
+	
+	
+
+}
