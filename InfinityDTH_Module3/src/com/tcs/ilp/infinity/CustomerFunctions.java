0c5e38bbb119b6bf7e4aa5c21ad9db278f90f52a
sindhimadhuri <sindhimadhuri6280@gmail.com>
2019-11-21 11:38:13 -0500

    Updated POJOs

---
+++ InfinityDTH_Module3/src/com/tcs/ilp/infinity/CustomerFunctions.java
@@ -0,0 +1,44 @@
+package com.tcs.ilp.infinity;
+import util.*;
+
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.text.AbstractDocument.Content;
+
+
+
+public class CustomerFunctions {
+	private Connection con=null;
+	private PreparedStatement pst=null;
+    private ResultSet rs=null;
+	public void displayCustomer() throws SQLException {
+		con=DButil.getConection();
+		if(con!=null){
+			String query="Select customername from customer";
+			
+			pst=con.prepareStatement(query);
+			rs=pst.executeQuery();
+				
+
+				if(rs.next()) {
+						int content1 = rs.getInt(1);
+						System.out.println(content1);
+					}
+					
+				}
+		
+		
+		
+		rs.close();
+		pst.close();
+		con.close();
+			
+	}
+}
+	
+
+
---
+++ InfinityDTH_Module3/src/com/tcs/ilp/infinity/Main.java
@@ -0,0 +1,12 @@
+package com.tcs.ilp.infinity;
+
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		CustomerFunctions cf = new CustomerFunctions();
+		cf.displayCustomer();
+	}
+
+}
